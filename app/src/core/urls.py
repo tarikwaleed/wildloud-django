9e2d6f22a839eb634633b72720e635a5b50b159c
tarikwaleed <tarikwaleed.tech@gmail.com>
2024-01-08 16:16:48 +0200

    :gear: static files settings

--- app/src/core/urls.py
+++ app/src/core/urls.py
@@ -1,6 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path(route="admin/", view=admin.site.urls),
+    path("", views.home, name="home-view"),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
